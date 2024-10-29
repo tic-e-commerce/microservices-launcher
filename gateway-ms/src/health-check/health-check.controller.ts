6232c7072664fbca88606d842142c8a63e362d6e
Leonel Otavalo <leoandy28aa@gmail.com>
2024-10-29 11:59:55 -0500

    Test feature branch

--- gateway-ms/src/health-check/health-check.controller.ts
+++ gateway-ms/src/health-check/health-check.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class HealthCheckController {
   @Get()
   healthCheck() {
-    return 'Client Gateway is up and running!';
+    return 'Client Gateway is up and running! Changes';
   }
 }
