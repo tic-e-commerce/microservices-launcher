d006aec8a72bfa2a792c71db54bba8cec319d05d
Gabriela Manobanda <150186087+mngabriela@users.noreply.github.com>
2024-11-11 18:28:55 -0500

    fix cart-ms

--- gateway-ms/src/app.module.ts
+++ gateway-ms/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { NatsModule } from './transports/nats.module';
 import { AuthModule } from './auth/auth.module';
 import { HealthCheckModule } from './health-check/health-check.module';
+import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [NatsModule, AuthModule, HealthCheckModule, CartModule],
+  imports: [NatsModule, AuthModule, HealthCheckModule, ProductsModule,CartModule],
 })
 export class AppModule {}
