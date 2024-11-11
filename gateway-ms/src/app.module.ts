b92389a2e6ad8e360920c7e834ef8c534199121c
Leonel Otavalo <leoandy28aa@gmail.com>
2024-11-11 18:37:46 -0500

    fix gateway

--- gateway-ms/src/app.module.ts
+++ gateway-ms/src/app.module.ts
@@ -3,8 +3,9 @@ import { NatsModule } from './transports/nats.module';
 import { AuthModule } from './auth/auth.module';
 import { HealthCheckModule } from './health-check/health-check.module';
 import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [NatsModule, AuthModule, HealthCheckModule, ProductsModule],
+  imports: [NatsModule, AuthModule, HealthCheckModule, ProductsModule, CartModule],
 })
 export class AppModule {}
