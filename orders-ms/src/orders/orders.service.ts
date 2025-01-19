7e3400d89333d3a7fdb3db1ab6fdf9984805ef25
Gabriela Manobanda <150186087+mngabriela@users.noreply.github.com>
2025-01-19 13:33:30 -0500

    Config deploy

--- orders-ms/src/orders/orders.service.ts
+++ orders-ms/src/orders/orders.service.ts
@@ -186,7 +186,7 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     return paymentSession;
   }
 
-  // UPDATE NOW
+  // UPDATE 
   // async findOrderById(order_id: string, includeOrderItems = true) {
   //   const order = await this.order.findUnique({
   //     where: { order_id },
